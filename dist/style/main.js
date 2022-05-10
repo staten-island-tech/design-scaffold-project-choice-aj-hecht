07200d9cbb979513cedc5f7c41aab2a189fb5c23
ajhecht <abigailh4@nycstudents.net>
2022-05-10 13:12:26 -0400

    5.10

--- dist/style/main.js
+++ dist/style/main.js
@@ -1,25 +1,18 @@
 import "../styles/style.css";
 
 const DOMSelectors = {
-  display: document.querySelector(".buttons"),
+  display: document.querySelector("buttons"),
 };
 
-document.querySelector(".theme").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
-  } else {
+document.querySelector("theme").addEventListener("click", function () {
+  if (document.body.classList.contains("warm")) {
     document.body.classList.add("cool");
     document.body.classList.remove("warm");
+  } else {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
   }
 });
-
-document.querySelector(".home").addEventListener("click", function () {
-  DOMSelectors.display.innerHTML = "";
-});
-document.querySelector(".blog").addEventListener("click", function () {
-  DOMSelectors.display.innerHTML = "";
-});
-document.querySelector(".log in").addEventListener("click", function () {
+document.querySelector("home").addEventListener("click", function () {
   DOMSelectors.display.innerHTML = "";
 });
