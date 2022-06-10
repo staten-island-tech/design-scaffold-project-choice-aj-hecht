f5ac330bb6cc7a25dbe38adbc5e5a6e865130ba5
ajhecht <abigailh4@nycstudents.net>
2022-06-10 13:09:42 -0400

    final

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import "../styles/style.css";
 
-const DOMSelectors = {
-  display: document.querySelector("buttons"),
-};
-
 document.querySelector(".theme").addEventListener("click", function () {
   if (document.body.classList.contains("warm")) {
     document.body.classList.add("cool");
